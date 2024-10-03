c8063fe24a3f483a16dad2fd15e6993e1cf05627
manojjha86 <68413139+manojjha86@users.noreply.github.com>
2024-10-03 18:05:35 +0530

    Update auth.js

--- client/src/apiCalls/auth.js
+++ client/src/apiCalls/auth.js
@@ -1,8 +1,10 @@
 import { axiosInstance } from './index';
 
+const url = 'https://quick-chat-server-8t9c.onrender.com';
+
 export const signupUser = async (user) => {
     try{
-        const response = await axiosInstance.post('/api/auth/signup', user);
+        const response = await axiosInstance.post(url + '/api/auth/signup', user);
         return response.data;
     }catch(error){
         return error;
@@ -11,7 +13,7 @@ export const signupUser = async (user) => {
 
 export const loginUser = async (user) => {
     try{
-        const response = await axiosInstance.post('/api/auth/login', user);
+        const response = await axiosInstance.post(url +'/api/auth/login', user);
         return response.data;
     }catch(error){
         return error;
