2a91eddab47c71f44480ed057f5d0fa670b7f57a
manojjha86 <68413139+manojjha86@users.noreply.github.com>
2024-10-03 18:07:22 +0530

    Update users.js

--- client/src/apiCalls/users.js
+++ client/src/apiCalls/users.js
@@ -1,8 +1,10 @@
 import { axiosInstance } from "./index";
 
+const url = 'https://quick-chat-server-8t9c.onrender.com';
+
 export const getLoggedUser = async () => {
     try{
-        const response = await axiosInstance.get('api/user/get-logged-user');
+        const response = await axiosInstance.get(url + 'api/user/get-logged-user');
         return response.data;
     }catch(error){
         return error;
@@ -11,7 +13,7 @@ export const getLoggedUser = async () => {
 
 export const getAllUsers = async () => {
     try{
-        const response = await axiosInstance.get('api/user/get-all-users');
+        const response = await axiosInstance.get(url + 'api/user/get-all-users');
         return response.data;
     }catch(error){
         return error;
@@ -20,7 +22,7 @@ export const getAllUsers = async () => {
 
 export const uploadProfilePic = async (image) => {
     try{
-        const response = await axiosInstance.post('api/user/upload-profile-pic', { image });
+        const response = await axiosInstance.post(url + 'api/user/upload-profile-pic', { image });
         return response.data;
     }catch(error){
         return error;
